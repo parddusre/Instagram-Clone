67e5dc2a4c03e95f3c7c7a1ce28842c1eff7d338
parddusre <parddusremortha@gmail.com>
2025-11-13 16:35:44 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Instagram-Clone/",
   plugins: [react()],
-  base:"/Instagram-Clone/",
-})
+});
+
+
 
